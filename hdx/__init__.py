ecfee1ffe435c009879448f6b39445e26e4287cc
Bayilla Geda <bayillag@gmail.com>
2026-05-04 11:14:41 +0300

    Create __init__.py

---
+++ hdx/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""HDX (Humanitarian Data Exchange) integration for Ethiopia Data Loader."""
+
+from .hdx_client import HDXClient
+from .hdx_browser import HDXBrowserDialog
+
+__all__ = ['HDXClient', 'HDXBrowserDialog']
