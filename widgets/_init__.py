33efc7d43253a8ce57f84513a66ab03b17acfb48
Bayilla Geda <bayillag@gmail.com>
2026-05-05 01:45:24 +0300

    Add __init__.py for widgets module

---
+++ widgets/_init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Dock widgets for Ethiopia Data Loader."""
+
+from .data_info_panel import DataInfoPanel
+from .search_panel import SearchPanel
+from .bookmarks_panel import BookmarksPanel
+from .statistics_panel import StatisticsPanel
+
+__all__ = ['DataInfoPanel', 'SearchPanel', 'BookmarksPanel', 'StatisticsPanel']
