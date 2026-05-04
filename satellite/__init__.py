5c8e8d18d1a57e89555d50b31391a0c92628f42f
Bayilla Geda <bayillag@gmail.com>
2026-05-04 16:46:29 +0300

    Create satellite module for Sentinel Hub integration
    Add initial implementation for satellite module with imports.

---
+++ satellite/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Sentinel Hub Satellite Imagery Integration for Ethiopia Data Loader."""
+
+from .sentinel_client import SentinelClient
+from .sentinel_browser import SentinelBrowserDialog
+
+__all__ = ['SentinelClient', 'SentinelBrowserDialog']
