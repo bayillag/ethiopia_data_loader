15debfa281c146ddf95d94d8efb169eeaf154b38
Bayilla Geda <bayillag@gmail.com>
2026-05-04 11:22:41 +0300

    Create __init__.py

---
+++ osm/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""OpenStreetMap/Overpass API Integration for Ethiopia Data Loader."""
+
+from .osm_client import OSMClient
+from .osm_browser import OSMBrowserDialog
+
+__all__ = ['OSMClient', 'OSMBrowserDialog']
