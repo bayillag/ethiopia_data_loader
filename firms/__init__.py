531660e7969856291e0ebc35aa8b89f248d94bb9
Bayilla Geda <bayillag@gmail.com>
2026-05-04 11:03:40 +0300

    Initialize firms module with client and browser dialog

---
+++ firms/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""NASA FIRMS Fire Data Integration for Sudan Data Loader."""
+
+from .firms_client import FIRMSClient
+from .firms_browser import FIRMSBrowserDialog
+
+__all__ = ['FIRMSClient', 'FIRMSBrowserDialog']
